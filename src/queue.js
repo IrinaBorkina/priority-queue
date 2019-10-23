f27cc225c7912a8d2de677a79299d3d2fce81485
Irina <irina_4ever@mail.ru>
2019-10-23 10:06:19 +0300

    feat: add part of solution

--- src/queue.js
+++ src/queue.js
@@ -2,23 +2,33 @@ const MaxHeap = require('./max-heap.js');
 
 class PriorityQueue {
 	constructor(maxSize) {
+		this.heap = new MaxHeap();
 
+		if(!maxSize) {
+			this.maxSize = 30;
+		} else {
+			this.maxSize = maxSize;
+		}
 	}
 
 	push(data, priority) {
-
+		if (this.size() >= this.maxSize) 
+			throw new Error;
+		return this.heap.push(data, priority);
 	}
 
 	shift() {
-
+		if (this.isEmpty()) 
+			throw new Error;
+		this.heap.pop();
 	}
 
 	size() {
-
+		return this.heap.size();
 	}
 
 	isEmpty() {
-		
+		return this.heap.isEmpty();
 	}
 }
 
